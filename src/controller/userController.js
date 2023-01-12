14832503799747c6499a99e46c122a78ce27419a
nmkhans <nurmoin05@gmail.com>
2023-01-12 12:26:00 +0600

    user login system created

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //? register a new user
 const registerUser = async (req, res) => {
@@ -46,7 +47,54 @@ const registerUser = async (req, res) => {
 
 //? login a user
 const loginUser = async (req, res) => {
+    try {
+        const data = req.body;
+        const userExist = await User.findOne({
+            email: data.email
+        });
 
+        if (userExist) {
+            const isMatch = await bcrypt.compare(data.password, userExist.password);
+
+            if (isMatch) {
+                const user = await User.findOne(
+                    { email: data.email },
+                    { password: 0 }
+                );
+
+                const token = jwt.sign(
+                    { email: user.email },
+                    process.env.TOKEN_SECRET,
+                    {expiresIn: "1h"}
+                );
+
+                res.status(200).json({
+                    success: true,
+                    message: "Login successfull.",
+                    token: token,
+                    data: user
+                })
+
+            } else {
+                res.status(500).json({
+                    success: false,
+                    message: "Incorrect password!"
+                })
+            }
+
+        } else {
+            res.status(500).json({
+                success: false,
+                message: "No user found!"
+            })
+        }
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "There was a server side error!"
+        })
+    }
 }
 
 module.exports = {
